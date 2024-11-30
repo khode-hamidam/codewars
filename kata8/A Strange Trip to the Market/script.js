d0f1d3529f5046b022c65f6aa2b99ad8eeab684f
hamid253 <ghorbani253@gmail.com>
2024-11-30 23:56:39 +0330

    aaa

--- kata8/A Strange Trip to the Market/script.js
+++ kata8/A Strange Trip to the Market/script.js
@@ -1,9 +1,5 @@
-let heightjohn=195;
-let massjohn=92;
-let heightMark=169;
-let massMark=78;
-let BmiJohn=(massjohn/heightjohn**2);
-let BmiMark=((heightMark**2)/massMark);
+function isLochNessMonster(s) {
 
-let BmiMarkhigh=BmiJohn>BmiMark;
-console.log(BmiJohn,BmiMark,BmiMarkhigh);
+    const patterns = ["tree fiddy", "3.50", "three fifty"];
+    return patterns.some(pattern => s.includes(pattern));
+}
