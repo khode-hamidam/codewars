f1e7b504a39db95edf1b61a210c6a3f0a9324d52
hamid253 <ghorbani253@gmail.com>
2024-11-30 23:53:28 +0330

    aaa

---
+++ kata8/A Strange Trip to the Market/script.js
@@ -0,0 +1,9 @@
+let heightjohn=195;
+let massjohn=92;
+let heightMark=169;
+let massMark=78;
+let BmiJohn=(massjohn/heightjohn**2);
+let BmiMark=((heightMark**2)/massMark);
+
+let BmiMarkhigh=BmiJohn>BmiMark;
+console.log(BmiJohn,BmiMark,BmiMarkhigh);
